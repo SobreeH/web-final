432d9bee73b0c40de26dbdca7474ddae554f501a
sobreeH <wansobree.hajihama@gmail.com>
2025-10-02 14:48:03 +0700

    routed to home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div
+      className="mx-4 sm:mx-[10%]" /* mx-4 = margin from x , sm:mx-[10%] = different screen size */
+    >
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
