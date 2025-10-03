f9f612d04a7c690238091563bbefbde933bc7bd9
sobreeH <wansobree.hajihama@gmail.com>
2025-10-03 14:42:05 +0700

    started with Headers

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      {/* ----- Left Side ------- */}
+      <div></div>
+      {/* ----- Right Side ------ */}
+      <div></div>
+    </div>
+  );
+};
+
+export default Header;
