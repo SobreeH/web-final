b14cbd0a305adfff02155d9e30c15b7852dc7f69
sobreeH <wansobree.hajihama@gmail.com>
2025-10-04 06:43:13 +0700

    created folder for frontend assets

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import { specialityData } from "../assets/frontend/assets";
 const SpecialityMenu = () => {
-  return <div></div>;
+  return (
+    <div id="seciality">
+      <h1>Find by Speciality</h1>
+      <p>
+        Simply browse through our extensive list of trusted doctors,schedule
+        your appointmet hassle-free
+      </p>
+      <div></div>
+    </div>
+  );
 };
 
 export default SpecialityMenu;
