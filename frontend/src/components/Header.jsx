2429403fe9e1b9e25f2c7a2b23008d620a56b364
sobreeH <wansobree.hajihama@gmail.com>
2025-10-03 16:52:04 +0700

    1h 1 min

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -21,7 +21,8 @@ const Header = () => {
           href=""
           className="flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300"
         >
-          Book appointment <img src={assets.arrow_icon} alt="" />
+          Book appointment{" "}
+          <img className="w-3" src={assets.arrow_icon} alt="" />
         </a>
       </div>
       {/* ----- Right Side ------ */}
