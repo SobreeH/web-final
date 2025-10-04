ce05f5917b724a4b3265e79b5a2b88b5f899c8f5
sobreeH <wansobree.hajihama@gmail.com>
2025-10-04 22:54:03 +0700

    starting related doctors

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { AppContext } from "../context/AppContext";
 import { assets } from "../assets/frontend/assets";
+import RelatedDoctors from "../components/RelatedDoctors";
 
 const Appointment = () => {
   const { docId } = useParams();
@@ -148,12 +149,27 @@ const Appointment = () => {
               ))}
           </div>
 
-          <div>
+          <div className="flex items-center gap-3 w-full overflow-x-scroll mt-4">
             {docSlots.length &&
               docSlots[slotIndex].map((item, index) => (
-                <p key={index}>{item.time.toLowerCase()}</p>
+                <p
+                  onClick={() => setSlotTime(item.time)}
+                  className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor-pointer ${
+                    item.time === slotTime
+                      ? "bg-blue-500 text-white"
+                      : "text-grey-400 border border-gray-300"
+                  }`}
+                  key={index}
+                >
+                  {item.time.toLowerCase()}
+                </p>
               ))}
           </div>
+          <button className="bg-blue-500 text-white text-sm font-light px-14 py-3 rounded-full my-6">
+            Book an appointment
+          </button>
+          {/* ----- Listing Related Doctors ----- */}
+          <RelatedDoctors docId={docId} speciality={docInfo.speciality} />
         </div>
       </div>
     )
