eb9ecd12193547e9da50394146471299572cff74
sobreeH <wansobree.hajihama@gmail.com>
2025-10-04 23:05:01 +0700

    Home , All Doctors , done

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -20,7 +20,10 @@ const TopDoctors = () => {
       >
         {doctors.slice(0, 10).map((item, index) => (
           <div
-            onClick={() => navigate(`/appointment/${item._id}`)}
+            onClick={() => {
+              navigate(`/appointment/${item._id}`);
+              scrollTo(0, 0);
+            }}
             className="border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10px] transition-all duration-500"
             key={index}
           >
