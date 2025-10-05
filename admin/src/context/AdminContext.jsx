b65874cd731fc23532939f47b07fd499595e6d7f
sobreeH <wansobree.hajihama@gmail.com>
2025-10-05 22:39:50 +0700

    show doctors working

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -1,5 +1,7 @@
+import axios from "axios";
 import { useState } from "react";
 import { createContext } from "react";
+import { toast } from "react-toastify";
 
 export const AdminContext = createContext();
 
@@ -7,11 +9,33 @@ const AdminContextProvider = (props) => {
   const [aToken, setAToken] = useState(
     localStorage.getItem("aToken") ? localStorage.getItem("aToken") : ""
   );
+  const [doctors, setDoctors] = useState([]);
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
+
+  const getAllDoctors = async () => {
+    try {
+      const { data } = await axios.post(
+        backendUrl + "/api/admin/all-doctors",
+        {},
+        { headers: { aToken } }
+      );
+      if (data.success) {
+        setDoctors(data.doctors);
+        console.log(data.doctors);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   const value = {
     aToken,
     setAToken,
     backendUrl,
+    doctors,
+    getAllDoctors,
   };
 
   return (
--- admin/src/pages/Admin/DoctorsList.jsx
+++ admin/src/pages/Admin/DoctorsList.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { useContext } from "react";
+import { AdminContext } from "../../context/AdminContext";
+import { useEffect } from "react";
 
 const DoctorsList = () => {
-  return <div>DoctorsList</div>;
+  const { doctors, aToken, getAllDoctors } = useContext(AdminContext);
+
+  useEffect(
+    (aToken) => {
+      getAllDoctors();
+    },
+    [aToken]
+  );
+
+  return (
+    <div className="m-5 max-h-[90vh] overflow-y-scroll">
+      <h1 className="text-lg font-medium">All Doctors</h1>
+      <div className="w-full flex flex-wrap gap-4 pt-5 gap-y-6">
+        {doctors.map((item, index) => (
+          <div key={index}>
+            <img src={item.image} alt="" />
+            <div>
+              <p>{item.name}</p>
+              <p> {item.speciality} </p>
+              <div>
+                <input type="checkbox" checked={item.available} />
+                <p>Available</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default DoctorsList;
