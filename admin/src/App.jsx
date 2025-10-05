e684ff16e54cd3b310325f3e2d794c3c725c6248
sobreeH <wansobree.hajihama@gmail.com>
2025-10-05 18:53:57 +0700

    sidebar functional

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -5,6 +5,11 @@ import { useContext } from "react";
 import { AdminContext } from "./context/AdminContext";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Admin/Dashboard";
+import AllApointments from "./pages/Admin/AllApointments";
+import AddDoctor from "./pages/Admin/AddDoctor";
+import DoctorsList from "./pages/Admin/DoctorsList";
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
@@ -15,6 +20,13 @@ const App = () => {
       <Navbar />
       <div className="flex items-start">
         <Sidebar />
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/admin-dashboard" element={<Dashboard />} />
+          <Route path="/all-appointments" element={<AllApointments />} />
+          <Route path="/add-doctor" element={<AddDoctor />} />
+          <Route path="/doctor-list" element={<DoctorsList />} />
+        </Routes>
       </div>
     </div>
   ) : (
