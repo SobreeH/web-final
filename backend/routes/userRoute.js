bad7459f9a624e7c5da5740b1d83a80a1d7ab5e6
sobreeH <wansobree.hajihama@gmail.com>
2025-10-06 18:59:41 +0700

    user CRU done

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,14 +3,24 @@ import {
   registerUser,
   loginUser,
   getProfile,
+  updateProfile,
 } from "../controllers/userController.js";
 import authUser from "../middlewares/authUser.js";
+import upload from "../middlewares/multer.js";
 
 const userRouter = express.Router();
 
+// login authen route
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
 
+// user profile route
 userRouter.get("/get-profile", authUser, getProfile);
+userRouter.post(
+  "/update-profile",
+  upload.single("image"),
+  authUser,
+  updateProfile
+);
 
 export default userRouter;
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -12,6 +12,7 @@ const AppContextProvider = (props) => {
   const [token, setToken] = useState(
     localStorage.getItem("token") ? localStorage.getItem("token") : false
   );
+  const [userData, setUserData] = useState(false);
 
   const getDoctorsData = async () => {
     try {
@@ -31,6 +32,23 @@ const AppContextProvider = (props) => {
     }
   };
 
+  const loadUserProfileData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/user/get-profile", {
+        headers: { token },
+      });
+      if (data.success) {
+        setUserData(data.userData);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      console.error("getDoctorsData error:", error);
+
+      toast.error(error.message);
+    }
+  };
+  // to show doctors
   useEffect(() => {
     getDoctorsData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -43,7 +61,20 @@ const AppContextProvider = (props) => {
     token,
     setToken,
     backendUrl,
+    userData,
+    setUserData,
+    loadUserProfileData,
   };
+
+  // to get user data
+
+  useEffect(() => {
+    if (token) {
+      loadUserProfileData();
+    } else {
+      setUserData(false);
+    }
+  }, [token]);
 
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
