f78e0706a2a637b5a38937a3bf0a4410ec77255c
sobreeH <wansobree.hajihama@gmail.com>
2025-10-06 08:16:15 +0700

    api for register new user complete backend 9 h

---
+++ backend/controllers/userController.js
@@ -0,0 +1,52 @@
+import validator from "validator";
+import bcrypt from "bcrypt";
+import userModel from "../models/userModel.js";
+import jwt from "jsonwebtoken";
+
+// api to register usr
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    // all fields not missing
+    if (!name || !password || !email) {
+      return res.json({ success: false, message: "Missing Detials" });
+    }
+    // validating email format
+    if (!validator.isEmail(email)) {
+      return res.json({ success: false, message: " Enter valid email " });
+    }
+
+    // validating password length
+    if (password.length < 8) {
+      return res.json({
+        success: false,
+        message: " Password must have at least 8 characters/Numbers ",
+      });
+    }
+
+    // hashing user password
+    const salt = await bcrypt.genSalt(5);
+    const hasedPassword = await bcrypt.hash(password, salt);
+
+    const userData = {
+      name,
+      email,
+      password: hasedPassword,
+    };
+
+    const newUser = new userModel(userData);
+    const user = await newUser.save();
+
+    // get _id for token to login user
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+    res.json({ success: true, token });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { registerUser };
