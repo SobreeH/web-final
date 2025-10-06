9b93d26947d618137d31ef213a7016ed35db731c
sobreeH <wansobree.hajihama@gmail.com>
2025-10-06 07:50:24 +0700

    frontend show doctors

--- backend/controllers/doctorController.js
+++ backend/controllers/doctorController.js
@@ -15,4 +15,15 @@ const ChangeAvailability = async (req, res) => {
   }
 };
 
-export { ChangeAvailability };
+const doctorList = async (req, res) => {
+  try {
+    const doctors = await doctorModel.find({}).select(["-password", "-email"]);
+
+    res.json({ success: true, doctors });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export { ChangeAvailability, doctorList };
