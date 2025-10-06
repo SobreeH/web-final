89058519623974e15f72c3b8212f8f182da7a89f
sobreeH <wansobree.hajihama@gmail.com>
2025-10-06 07:15:25 +0700

    removed from local storage

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,14 +1,24 @@
-import { createContext } from "react";
-import { doctors } from "../assets/assets";
+import React, { createContext, useState } from "react";
+import axios from "axios";
 
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
   const currencySymbol = "฿";
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
+  const [doctors, SetDoctors] = useState([]);
 
   const value = {
     doctors,
     currencySymbol,
+  };
+
+  const detDoctorsData = async () => {
+    try {
+      const { data } = await axios.get(backendUrl + "/api/doctor/list");
+      if (data.success) {
+      }
+    } catch (error) {}
   };
 
   return (
